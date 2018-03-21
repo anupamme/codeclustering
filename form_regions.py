0038a2d1fa9c8763dbd608cc8836f4d83a6cc6a3
Anupam Mediratta <mediratta@gmail.com>
2018-03-21 22:55:03 +0530

    first commit

--- form_regions.py
+++ form_regions.py
@@ -25,44 +25,69 @@ max_count = 20
 def vertical_more_imp(u, v):
     global count
     global max_count
-    coeff_x = (float)(count/max_count)
-    coeff_y = 1 - (float)(count/max_count)
+    coeff_x = 0
+    coeff_y = 1
 #    print('coeff x, y: ' + str(coeff_x) + ' : ' + str(coeff_y))
     return np.sqrt(coeff_x * 2 * np.power(u[0] - v[0],2) + coeff_y * 2 * np.power(u[1] - v[1],2))
 
-if __name__ == '__main__':
-    boxes = json.load(open('result.json', 'r'))['text_lines']
-    centroids = get_centroids(boxes)
-    X = preprocessing.StandardScaler().fit_transform(centroids)
+def balanced(u, v):
     global count
     global max_count
-    while count < max_count:
-        img = cv2.imread("img/input.png")
-        print('count, max_count: ' + str(count) + ' : ' + str(max_count))
+    coeff_x = 0.25
+    coeff_y = 0.75
+#    print('coeff x, y: ' + str(coeff_x) + ' : ' + str(coeff_y))
+    return np.sqrt(coeff_x * 2 * np.power(u[0] - v[0],2) + coeff_y * 2 * np.power(u[1] - v[1],2))
+
+def do_db_scan(boxes, index):
+    centroids = get_centroids(boxes)
+    X = preprocessing.StandardScaler().fit_transform(centroids)
+    img = cv2.imread("img/input.png")
+    if index == 0:
         distances = pairwise_distances(X, metric = vertical_more_imp)
-        db = cluster.DBSCAN(eps=0.15, min_samples=1, n_jobs=-3, metric='precomputed').fit(distances)
-        labels = db.labels_
-        # print(labels)
-        boxes = assign_labels(boxes, labels)
-        boxes = sorted(boxes, key=itemgetter('region'))
-        regions = []
-        mask_color_id = 0
-        color_list = colormap(rgb=True)
-        print("Number of cluster is {:2d}".format(len(set(db.labels_))))
-        for key, value in itertools.groupby(boxes, key=itemgetter('region')):
-            value = list(value)
-            left_xs = min(map(lambda x: int(np.round(x['x0'])), value))
-            right_xs = max(map(lambda x: int(np.round(x['x2'])), value))
-            top_ys = min(map(lambda x: int(np.round(x['y0'])), value))
-            bottom_ys = max(map(lambda x: int(np.round(x['y2'])), value))
-            tl = (left_xs, top_ys)
-            br = (right_xs, bottom_ys)
-            color = color_list[mask_color_id % len(color_list), 0:3]
-            color = list(map(int, color))
+    elif index == 1:
+        distances = pairwise_distances(X, metric = balanced)
+    db = cluster.DBSCAN(eps=0.15, min_samples=1, n_jobs=-3, metric='precomputed').fit(distances)
+    return img, db
+
+def print_rectangle(value, color_list, mask_color_id, cv2, img):
+    left_xs = min(map(lambda x: int(np.round(x['x0'])), value))
+    right_xs = max(map(lambda x: int(np.round(x['x2'])), value))
+    top_ys = min(map(lambda x: int(np.round(x['y0'])), value))
+    bottom_ys = max(map(lambda x: int(np.round(x['y2'])), value))
+    tl = (left_xs, top_ys)
+    br = (right_xs, bottom_ys)
+    color = color_list[mask_color_id % len(color_list), 0:3]
+    color = list(map(int, color))
+    cv2.rectangle(img, tl, br, color, 3)
+
+if __name__ == '__main__':
+    color_list = colormap(rgb=True)
+    boxes = json.load(open('result.json', 'r'))['text_lines']
+    img, db = do_db_scan(boxes, 0)
+    labels = db.labels_
+    # print(labels)
+    boxes = assign_labels(boxes, labels)
+    boxes = sorted(boxes, key=itemgetter('region'))
+    mask_color_id = 0
+    print("Number of cluster is {:2d}".format(len(set(db.labels_))))
+    for key, value in itertools.groupby(boxes, key=itemgetter('region')):
+        value = list(value)
+        print_rectangle(value, color_list, mask_color_id, cv2, img)        
+        mask_color_id += 1
+
+        # do next clustering
+        img2, db2 = do_db_scan(value, 1)
+        labels2 = db2.labels_
+        value = assign_labels(value, labels2)
+        value = sorted(value, key=itemgetter('region'))
+        print("Number of cluster is {:2d}".format(len(set(db2.labels_))))
+        for subkey, subvalue in itertools.groupby(value, key=itemgetter('region')):
+            subsubvalue = list(subvalue)
+            print_rectangle(subsubvalue, color_list, mask_color_id, cv2, img2) 
             mask_color_id += 1
-            cv2.rectangle(img, tl, br, color, 3)
-        cv2.imwrite('output_groups_' + str(count) + '.png', img)
-        count += 1
+            
+        cv2.imwrite('output_groups_.png', img)
+        cv2.imwrite('output_groups_2.png', img2)
     # img = cv2.resize(img, (1024, 1024))
     # cv2.imshow('img', img)
     # cv2.waitKey(0)
