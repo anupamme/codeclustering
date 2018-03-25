0da67ec7e8fe3691153f86b1ac934374e521a7d6
Anupam Mediratta <mediratta@gmail.com>
2018-03-25 18:09:21 +0530

    merge boxes

--- boxes_to_azure.py
+++ boxes_to_azure.py
@@ -21,17 +21,81 @@ import json
 
 import form_regions as form
 
-def merge_cluster_subsume(clusters: list):
-    pass
+font_dim = [5, 7]
 
-def merge_cluster_too_near(clusters: list):
-    pass
+'''
+1. sort the clusters vertically
+2. start from top and inspect each region
+3. if a region' bottom right is less than the bottom right of the parent then:
+    if the region's left and right are also within parent:
+        merge: add its polygons to the parent and mark it to be deleted.
+4. if the regions top is too close vertically merge the two clusters
+    
+1. sort the clusters horizontally
+2. start from left and move right incrementally
+3. if the two regions are too close horizontally merge them
+'''
+def merge_composite(clusters):
+    parent_cl_top = None
+    parent_cl_bottom = None
+    res = []
+    for cl in clusters:
+        top_l = cl['top_left']
+        bottom_r = cl['bottom_right']
+        if parent_cl_top != None and parent_cl_bottom != None:
+            if top_l[1] > parent_cl_top[1] and bottom_r[1] < parent_cl_bottom[1]:
+                # check for horizontal comp
+                if top_l[0] > parent_cl_top[0] and bottom_r[0] < parent_cl_bottom[0]:
+                    last_cl = res[-1]
+                    last_cl['polygons'] = last_cl['polygons'] + cl['polygons']
+                else:
+                    res.append(cl)
+            else:
+                res.append(cl)
+        else:
+            res.append(cl)
+        parent_cl_top = top_l
+        parent_cl_bottom = bottom_r
+    return res
+            
+def merge_adjacent (clusters, is_vertical):
+    parent_cl_bottom = None
+    res = []
+    for cl in clusters:
+        point_t = cl['top_left'][is_vertical]
+        point_b = cl['bottom_right'][is_vertical]
+        if parent_cl_bottom != None:
+            if point_t - parent_cl_bottom < font_dim[is_vertical]:
+                last_cl = res[-1]
+                last_cl['polygons'] = last_cl['polygons'] + cl['polygons']
+                last_cl['bottom_right'] = [max(last_cl['bottom_right'][0], cl['bottom_right'][0]), 
+                                            max(last_cl['bottom_right'][1], cl['bottom_right'][1])]
+            else:
+                res.append(cl)
+        else:
+            res.append(cl)
+        parent_cl_bottom = point_b
+    return res
 
-def convert_into_azure(clusters: list):
-    pass
+def merge_cluster(clusters: list):
+    # sort vertically
+    clusters.sort(key=lambda x: x['top_left'][1])
+    # merge composite
+    clusters_composite = merge_composite(clusters)
+    # merge adjacent
+    clusters_adjacent_v = merge_adjacent(clusters_composite, 1)
+    # sort horizontally
+    clusters_adjacent_v.sort(key=lambda x: x['top_left'][0])
+    # merge adjacent
+    clusters_adjacent_h = merge_adjacent(clusters_adjacent_v, 0)
+    return clusters_adjacent_h
+
 
 def do_clustering(boxes, img_name):
-    form.call(boxes, img_name)
+    outer, inner = form.call(boxes, img_name)
+    inner_compact = merge_cluster(inner)
+    return inner_compact
+    
     
 def call_ocr_api(img_name):
     url = 'http://ocr.callup.ai/ocr'
